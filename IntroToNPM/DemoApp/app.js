a532afe4921f37b486babf7c9c0825a5ce231184
Rohit Vinod <rohitmanjula@gmail.com>
2020-07-23 15:39:58 +0000

    IntroToNPM/

---
+++ IntroToNPM/DemoApp/app.js
@@ -0,0 +1,8 @@
+var something = require("cat-me");
+var jokes = require("knock-knock-jokes");
+var faker = require("faker");
+
+var prductName = faker.commerce.productName(); 
+var price = faker.commerce.price(); 
+
+console.log(productName,price);
