9c740bd852e75d426e25cb90e343235ba63201dc
Rohit Vinod <rohitmanjula@gmail.com>
2020-07-23 15:38:18 +0000

    DeployingDemo/

---
+++ DeployingDemo/app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("home");
+});
+
+app.get("/about", function(req, res){
+	res.render("about");
+});
+
+// app.listen(3000);
+app.listen(process.env.PORT, process.env.IP);
